9fb28efc8471676ec8b52a07fb68b486713266bf
Theodore Nordsieck <theo@opscode.com>
2013-09-09 17:33:58 -0700

    Converted plugins/solaris2/virtualization to Mixlib::ShellOut.

--- spec/unit/plugins/solaris2/virtualization_spec.rb
+++ spec/unit/plugins/solaris2/virtualization_spec.rb
@@ -20,6 +20,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper.rb')
 
 describe Ohai::System, "Solaris virtualization platform" do
   before(:each) do
+    @psrinfo_pv = <<-PSRINFO_PV
+The physical processor has 1 virtual processor (0)
+  x86 (GenuineIntel family 6 model 2 step 3 clock 2667 MHz)
+        Intel Pentium(r) Pro
+PSRINFO_PV
+
     @plugin = get_plugin("solaris2/virtualization")
     @plugin[:os] = "solaris2"
     @plugin.extend(SimpleFromFile)
@@ -33,28 +39,22 @@ describe Ohai::System, "Solaris virtualization platform" do
   describe "when we are checking for kvm" do
     before(:each) do
       File.should_receive(:exists?).with("/usr/sbin/psrinfo").and_return(true)
-      @stdin = double("STDIN", { :close => true })
-      @pid = 10
-      @stderr = double("STDERR")
-      @stdout = double("STDOUT")
-      @status = 0
     end
 
     it "should run psrinfo -pv" do
-      @plugin.should_receive(:popen4).with("/usr/sbin/psrinfo -pv").and_return(true)
+      @plugin.should_receive(:shell_out).with("/usr/sbin/psrinfo -pv")
       @plugin.run
     end
 
     it "Should set kvm guest if psrinfo -pv contains QEMU Virtual CPU" do
-      @stdout.stub(:read).and_return("QEMU Virtual CPU") 
-      @plugin.stub(:popen4).with("/usr/sbin/psrinfo -pv").and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+      @plugin.stub(:shell_out).with("/usr/sbin/psrinfo -pv").and_return([0, "QEMU Virtual CPU", ""])
       @plugin.run
       @plugin[:virtualization][:system].should == "kvm"
       @plugin[:virtualization][:role].should == "guest"
     end
 
     it "should not set virtualization if kvm isn't there" do
-      @plugin.should_receive(:popen4).with("/usr/sbin/psrinfo -pv").and_return(true)
+      @plugin.should_receive(:shell_out).with("/usr/sbin/psrinfo -pv").and_return([0, @psrinfo_pv, ""])
       @plugin.run
       @plugin[:virtualization].should == {}
     end
@@ -63,15 +63,10 @@ describe Ohai::System, "Solaris virtualization platform" do
   describe "when we are parsing smbios" do
     before(:each) do
       File.should_receive(:exists?).with("/usr/sbin/smbios").and_return(true)
-      @stdin = double("STDIN", { :close => true })
-      @pid = 20
-      @stderr = double("STDERR")
-      @stdout = double("STDOUT")
-      @status = 0
     end
 
     it "should run smbios" do
-      @plugin.should_receive(:popen4).with("/usr/sbin/smbios").and_return(true)
+      @plugin.should_receive(:shell_out).with("/usr/sbin/smbios")
       @plugin.run
     end
 
@@ -88,9 +83,7 @@ ID    SIZE TYPE
   UUID: D29974A4-BE51-044C-BDC6-EFBC4B87A8E9
   Wake-Up Event: 0x6 (power switch)
 MSVPC
-      @stdout.stub(:read).and_return(ms_vpc_smbios) 
-       
-      @plugin.stub(:popen4).with("/usr/sbin/smbios").and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+      @plugin.stub(:shell_out).with("/usr/sbin/smbios").and_return([0, ms_vpc_smbios, ""])
       @plugin.run
       @plugin[:virtualization][:system].should == "virtualpc"
       @plugin[:virtualization][:role].should == "guest"
@@ -110,14 +103,14 @@ ID    SIZE TYPE
   Wake-Up Event: 0x6 (power switch)
 VMWARE
       @stdout.stub(:read).and_return(vmware_smbios)
-      @plugin.stub(:popen4).with("/usr/sbin/smbios").and_yield(@pid, @stdin, @stdout, @stderr).and_return(@status)
+      @plugin.stub(:shell_out).with("/usr/sbin/smbios").and_return([0, vmware_smbios, ""])
       @plugin.run
       @plugin[:virtualization][:system].should == "vmware"
       @plugin[:virtualization][:role].should == "guest"
     end
 
     it "should run smbios and not set virtualization if nothing is detected" do
-      @plugin.should_receive(:popen4).with("/usr/sbin/smbios").and_return(true)
+      @plugin.should_receive(:shell_out).with("/usr/sbin/smbios")
       @plugin.run
       @plugin[:virtualization].should == {}
     end
